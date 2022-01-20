7b672302da8c241c593bec177f726407f0af08d0
Alina Rybina <alina.rybina@gmail.com>
2022-01-21 00:52:31 +0200

    withoutSubMenu

---
+++ 2-artWebsite/js/base.js
@@ -0,0 +1,82 @@
+let dataArts = [
+    {
+        article: '1',
+        mainImage: 'assets/images/1.png',
+        titleArt: 'an interesting title',
+        artist: 'an interesting artist',
+        description: 'an interesting description',
+        fulldescription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas recusandae cupiditate quo exercitationem maiores animi.',
+        price: 620,
+        type: 'painting',
+        images: ['assets/images/1.png'],
+        link: '',
+    },
+    {
+        article: '2',
+        mainImage: 'assets/images/2.png',
+        titleArt: 'an interesting title',
+        artist: 'an interesting artist',
+        description: 'an interesting description',
+        fulldescription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas recusandae cupiditate quo exercitationem maiores animi.',
+        price: 620,
+        type: 'painting',
+        images: ['assets/images/2.png'],
+        link: '', 
+    },
+    {
+        article: '3',
+        mainImage: 'assets/images/3.png',
+        titleArt: 'an interesting title',
+        artist: 'an interesting artist',
+        description: 'an interesting description',
+        fulldescription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas recusandae cupiditate quo exercitationem maiores animi.',
+        price: 620,
+        type: 'painting',
+        images: ['assets/images/3'],
+        link: '', 
+    },
+    {
+        article: '4',
+        mainImage: 'assets/images/4.png',
+        titleArt: 'an interesting title',
+        artist: 'an interesting artist',
+        description: 'an interesting description',
+        fulldescription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas recusandae cupiditate quo exercitationem maiores animi.',
+        price: 620,
+        type: 'painting',
+        images: ['assets/images/4'],
+        link: '', 
+    },
+    {
+        article: '5',
+        mainImage: 'assets/images/5.png',
+        titleArt: 'an interesting title',
+        artist: 'an interesting artist',
+        description: 'an interesting description',
+        fulldescription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas recusandae cupiditate quo exercitationem maiores animi.',
+        price: 620,
+        type: 'painting',
+        images: ['assets/images/5'],
+        link: '', 
+    },
+]
+
+
+let typeProduct = [
+    {
+        mainMenu: {title: 'paintings', link: '#'},
+        subMenu: [{title: 'landscapes', link: '#'}]
+    },
+    {
+        mainMenu: {title: 'drawings', link: '#'},
+        subMenu: [{title: 'landscapes', link: '#'}]
+    },
+    {
+        mainMenu: {title: 'sculpture', link: '#'},
+        subMenu: [{title: 'landscapes', link: '#'}]
+    },
+    {
+        mainMenu: {title: 'artists', link: '#'},
+        subMenu: [{title: 'landscapes', link: '#'}]
+    },
+]
---
+++ 2-artWebsite/js/render.js
@@ -0,0 +1,66 @@
+const $ = {}
+ 
+$.createProductSlider = dataArts => {
+    let innerTxt = '';
+    dataArts.forEach(art => {
+        innerTxt = `
+            <div class="art-window" id=${art.article} >
+                <img src=${art.mainImage} alt=${art.titleArt} class="mainImage">
+                <h3 class="titleArt">${art.titleArt}</h3>
+                <h2 class="artist">${art.artist}</h2>
+                <h2 class="description">${art.description}</h2>
+                <div class="desc-wrap">
+                <h2 class="price">${art.price}$</h2>
+                <div class="desc-wrap-icon">
+                <a href="#" class="descIconLink"> <img src="assets/images/button-buy.svg" alt="" class="descIconImg"></a>
+<a href="#" class="descIconLink"> <img src="assets/images/button-share.svg" alt="" class="descIconImg"></a>
+                </div>
+                </div>
+            </div>
+        `
+        document.querySelector('.slider').insertAdjacentHTML('beforeend', innerTxt);
+    });
+}
+
+$.deleteProductFromSlide = id => {
+    document.querySelector('.slider').removeChild(document.getElementById(`${id}`))
+}
+
+$.createMenu = typeProduct => {
+    let innerTxt = '';
+    typeProduct.forEach(type => {
+        innerTxt = `
+            <li class="menuTitle">
+                <a href=${type.mainMenu.link} class="mainMenuLink">${type.mainMenu.title}</a>
+                <ul class="sub-menu">
+        `
+
+        type.subMenu.forEach(subColumn => {
+            innerTxt += `
+            <li class="subMenuTitle">
+                <a href=${subColumn.link} class="subMenuLink">${subColumn.title}</a>
+            </li>
+            `
+        });
+
+        innerTxt += `
+            </ul>
+        </li>
+        `
+        document.querySelector('.nav').insertAdjacentHTML('beforeend', innerTxt);
+    })
+}
+$.createMenu(typeProduct)
+$.createProductSlider(dataArts)
+
+console.log(document.querySelector('.footer').parentNode.offsetWidth);
+// mainMenu: {title: 'paintings', link: '#'},
+// subMenu: [{title: 'landscapes', link: '#'}]
+
+function vh (x){
+    console.log(`${(100*x/1039).toFixed(2)}vh`);
+}
+
+function vw (x){
+    console.log(`${(100*x/1140).toFixed(2)}vw`);
+}
