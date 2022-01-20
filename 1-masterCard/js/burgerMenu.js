e376b8254b9a60bd6f34aeb0312665273431cd2d
Alina Rybina <alina.rybina@gmail.com>
2022-01-20 12:45:29 +0200

    fix

---
+++ 1-masterCard/js/burgerMenu.js
@@ -0,0 +1,15 @@
+// function createBrgMenu(options){
+//     options.map(name => {
+//         document.querySelector('.brg-menu').insertAdjacentHTML('beforeend', createContentMenu(name.title))
+//     });
+// };
+
+
+// createBrgMenu(options);
+// function createContentMenu(element){
+//     let content = '';
+//         content += `<a href="http://#">${element}</a><hr>
+//         `;
+//     return content;
+// }
+
--- 1-masterCard/js/menu.js
+++ 1-masterCard/js/menu.js
@@ -1,21 +1,46 @@
 const menu = {};
 
+
+console.log(window.innerHeight);
+console.log(document.querySelector('.menu li').offsetWidth);
 let propertiesItem = document.querySelectorAll('.menu li')
 
 let prop = new Object;
 
-document.querySelector('.menu').addEventListener('mouseover', () =>{
+    document.querySelector('.menu').addEventListener('mouseover', () =>{
     propertiesItem = document.querySelectorAll('.menu li')
+
     propertiesItem.forEach(item => {
         prop[`${item.innerText}`] = new Object;
+        const koefWidth = (100/window.innerWidth)
+        const koefHeight = (100/window.innerHeight)
         let padding = parseInt(window.getComputedStyle(item, null).getPropertyValue('padding-right'))
-        prop[`${item.innerText}`].width = `${item.offsetWidth}px`
-        prop[`${item.innerText}`].left = `${item.getBoundingClientRect().x + padding}px`
-        prop[`${item.innerText}`].height = `${parseInt(item.getBoundingClientRect().y) + parseInt(item.offsetHeight)}px`
+        prop[`${item.innerText}`].width = `${item.offsetWidth*koefWidth}vw`
+        console.log(prop[`${item.innerText}`].width);
+        prop[`${item.innerText}`].left = `${(item.getBoundingClientRect().x+padding)*koefWidth}vw`
+        prop[`${item.innerText}`].height = `${parseInt(item.getBoundingClientRect().y)*koefHeight}vh`
+        prop[`${item.innerText}`].paddingTop = `${parseInt(item.offsetHeight*koefHeight)}vh`
         let id = item.innerText.split(' ').join('');
         prop[`${item.innerText}`].title = `"${id}"`;
     })
+
 })
+
+
+//     propertiesItem = document.querySelectorAll('.menu li')
+
+//     propertiesItem.forEach(item => {
+//         prop[`${item.innerText}`] = new Object;
+//         let padding = parseInt(window.getComputedStyle(item, null).getPropertyValue('padding-right'))
+//         prop[`${item.innerText}`].width = `${item.offsetWidth}px`
+//         prop[`${item.innerText}`].left = `${item.getBoundingClientRect().x + padding}px`
+//         prop[`${item.innerText}`].height = `${parseInt(item.getBoundingClientRect().y) + parseInt(item.offsetHeight)}px`
+//         let id = item.innerText.split(' ').join('');
+//         prop[`${item.innerText}`].title = `"${id}"`;
+//     })
+// })
+
+
 
 
 
@@ -58,17 +83,18 @@ menu.signUpClose = () => {
 
 menu.create = option => {
         let list = `
-        <div class="list" id=${prop[option.title].title} style="width:${prop[option.title].width}; left:${prop[option.title].left}; top:${prop[option.title].height}; position:fixed">
+        <div class="list" id=${prop[option.title].title} style="width:${prop[option.title].width}; left:${prop[option.title].left}; top:${prop[option.title].height}; padding-top:${prop[option.title].paddingTop}">
             ${createContent(option.content)}
         </div>
         `
+        console.log(list);
         document.querySelector('.menu').insertAdjacentHTML('afterend', list)
 }
 
-function createContent (element){
+function createContent(element){
     let content = '';
     element.forEach(e => {
-        content += `<a href="http://#">${e}</a><hr>
+        content += `<a href="http://#">${e}</a>
         `;
     })
     return content;
@@ -77,23 +103,17 @@ function createContent (element){
 
 document.querySelector('.menu').addEventListener('mouseover', event =>{
     let checkOption = options.find(element => element.title ===  event.target.innerText)
-    // if(document.querySelector('.list')) document.querySelector('.nav').removeChild(document.querySelector('list'))
+    if(document.querySelector('.list')) document.querySelector('.nav').removeChild(document.querySelector('.list'))
     if(!document.getElementById(`${checkOption.title.split(' ').join('')}`)) menu.create(checkOption);
-    console.log((document.querySelector('.list')));
 
 
 
     const outOption = document.getElementById(`${checkOption.title.split(' ').join('')}`)
-    document.querySelector('.menu').addEventListener('mouseout', () => {
-        window.addEventListener('mouseover', event => {
-            // console.log(outOption);
-                // console.log(event.target.parentNode!==outOption);
+    document.querySelector('.list').addEventListener('mouseleave', () => {
 
-                if (event.target!==outOption&&event.target.parentNode!==outOption){
+        document.querySelector('.list').style.display = 'none';
+            document.querySelector('.nav').removeChild(document.querySelector('.list'))
+    })
 
-                    document.querySelector('.nav').removeChild(outOption)
-                };        
-            })
-        })
 })
 
