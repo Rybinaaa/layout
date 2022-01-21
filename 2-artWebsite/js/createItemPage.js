190af64fe63328882df4b6c565bae44a179fc32f
Alina Rybina <alina.rybina@gmail.com>
2022-01-21 17:40:56 +0200

    pages

---
+++ 2-artWebsite/js/createItemPage.js
@@ -0,0 +1,65 @@
+// let dataArts = [
+//     {
+//         article: '1',
+//         mainImage: 'assets/images/1.png',
+//         titleArt: 'an interesting title',
+//         artist: 'an interesting artist',
+//         description: 'an interesting description',
+//         fulldescription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas recusandae cupiditate quo exercitationem maiores animi.',
+//         price: 620,
+//         type: 'painting',
+//         images: ['assets/images/1.png'],
+//         link: '',
+//     },
+
+
+
+
+let htmlCode = `
+<header class="header">
+        <div class="container-head">
+            <img src="assets/images/logo-cultured-kid.svg" alt="" class="logo">
+            <div class="menu">
+                <ul class="nav">
+               
+                </ul>
+            
+
+                <img src="assets/images/black.svg" alt="" class="menuIcon">
+                <img src="assets/images/black Copy.svg" alt="" class="menuIcon">
+  
+            </div>
+        </div>
+    </header>
+    <main class="main">
+        <div class="container-item">
+                <div class="paintView">
+                    <div class="descImg">
+                        <img src="assets/images/2.png" alt="" class="paint">
+                        <img src="assets/images/2.png" alt="" class="paint">
+                        <img src="assets/images/2.png" alt="" class="paint">
+                    </div>
+                    <div class="paintView-wrap">
+                        <img src="assets/images/2.png" alt="" class="mainImg">
+                        <div class="options">
+                            <img src="assets/images/button-view.svg" alt="">
+                            <img src="assets/images/button-share (1).svg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="paintDesk">
+                    <img class="back" src="assets/images/back.svg" alt="">
+                    <h1 class="titleItem">Wallowing Breeze </h1>
+                    <h3 class="itemAuthor">Hettie Richards</h3>
+                    <h3 class="itemDesk">Dynamic and elusive abstraction and texture. Plays between the lines of chaos and serenity. Perfect fit for modern and contemporary styled interiors.</h3>
+                    <h3 class="price">620$</h3>
+                    <img src="assets/images/location.svg" alt="">
+                    <img src="assets/images/estimated shipping.svg" alt="">
+                    <button class="addToCard">Add to card</button>
+
+                    
+                </div>
+        </div>
+    </main>
+
+`
--- 2-artWebsite/js/scrollSlider.js
+++ 2-artWebsite/js/scrollSlider.js
@@ -1,9 +1,30 @@
-document.querySelector('.slider-icon').addEventListener('click', () => {
+$.scroll = () => {
     const windowWidth = window.innerWidth;
     const slider = document.querySelector('.slider');
     const scrollWidth = slider.scrollWidth;
     const maxScroll = scrollWidth - windowWidth;
     slider.insertAdjacentElement('beforeend', slider.firstElementChild)
     slider.scrollBy(maxScroll, 0)
+}
 
+
+
+document.querySelector('.slider-icon').addEventListener('click', () => {
+    // const windowWidth = window.innerWidth;
+    // const slider = document.querySelector('.slider');
+    // const scrollWidth = slider.scrollWidth;
+    // const maxScroll = scrollWidth - windowWidth;
+    // slider.insertAdjacentElement('beforeend', slider.firstElementChild)
+    // slider.scrollBy(maxScroll, 0)
+    $.scroll()
+})
+
+let autoScroll = setInterval($.scroll, 2000)
+
+document.querySelector('.slider').addEventListener('mouseover', () => {
+    clearInterval(autoScroll)
+})
+
+document.querySelector('.slider').addEventListener('mouseout', () => {
+    autoScroll = setInterval($.scroll, 2000)
 })
